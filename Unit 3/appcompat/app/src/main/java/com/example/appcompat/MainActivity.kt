065bdee141f37134446ff663ce322b1172cf53be
manh <ongnoitv3@gmail.com>
2025-10-01 06:45:34 +0700

    create appcompat with dice roll

---
+++ Unit 3/appcompat/app/src/main/java/com/example/appcompat/MainActivity.kt
@@ -0,0 +1,62 @@
+package com.example.appcompat
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
+import com.example.appcompat.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        diceImage = findViewById(R.id.dice_image)
+        val rollButton = findViewById<AppCompatButton>(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    fun rollDice() {
+        val randomInt = (1..6).random()
+        val imageSource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(imageSource)
+
+    }
+}
