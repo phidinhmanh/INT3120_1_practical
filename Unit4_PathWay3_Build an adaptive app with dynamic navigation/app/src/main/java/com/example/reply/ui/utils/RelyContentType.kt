de83ee0e4a04e59c78f75959b2fca9224ad01164
manh <ongnoitv3@gmail.com>
2025-10-06 08:53:30 +0700

    adaptive+navigation

---
+++ Unit4_PathWay3_Build an adaptive app with dynamic navigation/app/src/main/java/com/example/reply/ui/utils/RelyContentType.kt
@@ -0,0 +1,7 @@
+package com.example.reply.ui.utils
+
+enum class ReplyContentType {
+    LIST_AND_DETAIL,
+    LIST_ONLY,
+    DETAIL_ONLY
+}
---
+++ Unit4_PathWay3_Build an adaptive app with dynamic navigation/app/src/main/java/com/example/reply/ui/utils/WindowStateUtils.kt
@@ -0,0 +1,9 @@
+package com.example.reply.ui.utils
+
+enum class ReplyNavigationType {
+    NAVIGATION_RAIL,
+    BOTTOM_NAVIGATION,
+    PERMANENT_NAVIGATION_DRAWER
+}
+
+
